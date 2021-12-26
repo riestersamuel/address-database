7f22071d8280adf5e2fe3b6634beb7e0dc372ef8
Lisa <ls228@hdm-stuttgart.de>
2021-12-26 21:41:22 +0100

    Writer Klasse bearbeitet um Name, Mail und Nummer einzutragen

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -3,11 +3,15 @@ import java.io.IOException;
 // Klasse um Text in einer Datei abzuspeichern
 public class Writer {
         public static void main(String[] args) {
+            NewPerson person1 = new NewPerson();
+            String name = person1.name;
+            String mail = person1.mail;
+            String number= person1.number;
             try {
                 // neuer filewriter wird erstellt
                 FileWriter myWriter = new FileWriter("filewriter.txt");
                 // hier kann man den Text schreiben, der in der Datei gespeichert werden soll
-                myWriter.write("neuer Text");
+                myWriter.write(name + ", " + mail + ", " + number);
                 //schließt den filewriter
                 myWriter.close();
             } catch (IOException e) {
