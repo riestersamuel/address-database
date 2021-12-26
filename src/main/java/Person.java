bc0c1f3b926a5c204d51fc0d94776839045de7bd
Lisa Kohls <lk210@hdm-stuttgart.de>
2021-12-26 16:50:39 +0100

    Neue Person hinzufügen als Methode

---
+++ src/main/java/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+
+    public static void main(String[] args) {
+        final NewPerson person = new NewPerson();
+       // final DeletePerson deletePerson = DeletePerson();
+    }
+}
