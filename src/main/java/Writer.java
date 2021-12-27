a65a5a518b90ff0dddbd46949985530724f8847c
Lisa <ls228@hdm-stuttgart.de>
2021-12-27 17:19:15 +0100

    Writer geändert, damit mehrere Einträge gespeichert werden können

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -1,23 +1,25 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+
 // Klasse um Text in einer Datei abzuspeichern
 public class Writer {
-        public Writer () {
-            NewPerson person1 = new NewPerson();
-            String name = person1.name;
-            String mail = person1.mail;
-            String number= person1.number;
-            //Klasse NewPerson wird aufgerufen, um neue Einträge zu erstellen.
-            try {
-                // neuer Filewriter wird erstellt
-                FileWriter myWriter = new FileWriter("filewriter.csv");
-                // Die Einträge werden in einer Datei gespeichert.
-                myWriter.write(name + ", " + mail + ", " + number);
-                //schließt den Filewriter
-                myWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public Writer() {
+        NewPerson person1 = new NewPerson();
+        String name = person1.name;
+        String mail = person1.mail;
+        String number = person1.number;
+
+        try {
+            // neuer filewriter wird erstellt
+            FileWriter myWriter = new FileWriter("filewriter.csv",true);
+            // hier kann man den Text schreiben, der in der Datei gespeichert werden soll
+            myWriter.write( name + ", " + mail + ", " + number + " " + '\n');
+
+            //schließt den filewriter
+            myWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+}
