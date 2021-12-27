4d577f5bde51940323ba08b83dedcbb0a2bf2a95
Lisa Kohls <lk210@hdm-stuttgart.de>
2021-12-27 10:53:21 +0100

    Array Person hinzufügen

--- src/main/java/NewPerson.java
+++ src/main/java/NewPerson.java
@@ -3,18 +3,22 @@ public class NewPerson {
     String name;
     String mail;
     String number;
+    final String[] data = {"name","mail","number"};
     //Konstruktor um eine neue Person hinzuzufügen
     public NewPerson(){
 
         try (final Scanner scan = new Scanner(System.in)) {
             System.out.println("Enter your name: ");
             this.name = scan.next();
+            data[0]=this.name;
             System.out.println(name);
             System.out.println("Enter your mail address: ");
             this.mail = scan.next();
+            data[1]=mail;
             System.out.println("mail: "+mail);
             System.out.println("Enter your phone number: ");
             this.number = scan.next();
+            data[2]=number;
             System.out.println("phone: "+number);
         }
         }
