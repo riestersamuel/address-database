e47eb60fbe93a066126a83dfc4cc482ee0c44a56
Lisa Kohls <lk210@hdm-stuttgart.de>
2021-12-27 20:09:10 +0100

    Person aus Datei löschen

--- src/main/java/NewPerson.java
+++ src/main/java/NewPerson.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 public class NewPerson {
     String name, mail, number;
-    final String[] data = {"name","mail","number"};
 
     //Konstruktor um eine neue Person hinzuzufügen
     public NewPerson(){
         try (final Scanner scan = new Scanner(System.in)) {
             System.out.println("Enter your name: ");
             this.name = scan.next();
-            data[0]=this.name;
             System.out.println(name);
             System.out.println("Enter your mail address: ");
             this.mail = scan.next();
-            data[1]=mail;
             System.out.println("mail: "+mail);
             System.out.println("Enter your phone number: ");
             this.number = scan.next();
-            data[2]=number;
             System.out.println("phone: "+number);
             /* Ansatz um die Daten zu speichern:
             String personInfo= name+", "+mail+", "+number;
