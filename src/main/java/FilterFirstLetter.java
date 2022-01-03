44f0e6b9a9d47f3ed385d0fe66d93d99538cf61d
riestersamuel <riestersamuel@icloud.com>
2022-01-03 18:14:32 +0100

    Bugfix überarbeitet und verbessert

--- src/main/java/FilterFirstLetter.java
+++ src/main/java/FilterFirstLetter.java
@@ -11,36 +11,14 @@ public class FilterFirstLetter {
         String letter;
         System.out.println("\nEnter the first letter of the person's last name you're searching for: ");
         letter = scan.next();
-        System.out.println("\nThese matching entries were found ... ");
         try {
+            //Versuch 2
+            //Idee: Wenn keine Einträge für den Buchstaben gefunden wurden, dann lass es den Nutzer wissen.
             String myFileName = "address.txt";
             int line;
 
             //neue Liste mit allen Einträgen der Textdatei wird aufgerufen
             List<String> allFileEntries = Files.readAllLines(Paths.get(myFileName));
-
-            //solange i kleiner ist als die Anzahl der Zeilen wird der Name gesucht
-            for (int i = 0; i < allFileEntries.size(); i++) {
-                String myNextLine = allFileEntries.get(i).toUpperCase();
-                String[] newWord = myNextLine.split(" ");
-                if (newWord[1].startsWith(letter.toUpperCase())) {
-                    line = i;//Zeile gefunden
-                    System.out.println(allFileEntries.get(line));
-                }
-
-            }
-
-            // wenn Buchstabe der gesucht wurde nicht existiert
-            /*for (int i = 0; i < allFileEntries.size(); i++) {
-                String myNextLine = allFileEntries.get(i);
-                if (!(myNextLine.startsWith(letter.toUpperCase()))) {
-                    System.out.println("Sorry, no matching entries were found!");
-                    break;
-                }
-            }*/
-
-            //Versuch 2
-            //Idee: Wenn keine Einträge für den Buchstaben gefunden wurden, dann lass es den Nutzer wissen.
             boolean atLeastOneMatchingEntry = false;
             for (int i = 0; i < allFileEntries.size(); i++) {
                 String dataAtLineI = allFileEntries.get(i).toUpperCase();
@@ -55,6 +33,26 @@ public class FilterFirstLetter {
             if (!atLeastOneMatchingEntry) {
                 System.out.println("\nSorry, no matching entries were found.");
             }
+            else {System.out.println("\nThese matching entries were found ... ");
+                //solange i kleiner ist als die Anzahl der Zeilen wird der Name gesucht
+                for (int i = 0; i < allFileEntries.size(); i++) {
+                    String myNextLine = allFileEntries.get(i).toUpperCase();
+                    String[] newWord = myNextLine.split(" ");
+                    if (newWord[1].startsWith(letter.toUpperCase())) {
+                        line = i;//Zeile gefunden
+                        System.out.println(allFileEntries.get(line));
+                    }}
+
+            }
+
+            // Versuch 1: Wenn Buchstabe der gesucht wurde nicht existiert
+            /*for (int i = 0; i < allFileEntries.size(); i++) {
+                String myNextLine = allFileEntries.get(i);
+                if (!(myNextLine.startsWith(letter.toUpperCase()))) {
+                    System.out.println("Sorry, no matching entries were found!");
+                    break;
+                }
+            }*/
 
             //Fehlermeldung, wenn Datei nicht gefunden wird
         } catch (IOException ex) {
